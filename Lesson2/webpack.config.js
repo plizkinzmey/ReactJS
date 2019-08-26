e86fbe4a9d85bfdc506cf98d998f88dbffd24396
Aleksandr Babkin <plizkinzmey@gmail.com>
2019-08-26 19:54:22 +0300

    Домашняя работа второго урока.

---
+++ Lesson2/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path'),
+    htmlPlugin = require('html-webpack-plugin'),
+    miniCssExtractPlugin = require('mini-css-extract-plugin'),
+    uglifyJsPligin = require('uglifyjs-webpack-plugin');
+
+    module.exports = {
+        entry: path.resolve(__dirname,'src','index.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        devServer: {
+            historyApiFallback: true,
+            contentBase: path.resolve(__dirname, 'dist')
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        miniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                    ],
+                },
+                {
+                    test: /\.(.gif|png|jpe?g)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: 'img/[hash.[ext]'
+                            }
+                        }
+                    ]
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.jsx'],
+        },
+        plugins: [
+            new htmlPlugin({
+                template: path.resolve(__dirname, 'src', 'index.html'),
+                filename: 'index.html'
+            }),
+        ],
+    };
