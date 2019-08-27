2ff8346fed0c05064023a9845b0cad8d03d59b3f
Aleksandr Babkin <plizkinzmey@gmail.com>
2019-08-27 23:02:29 +0300

    мелочи

--- Lesson2/src/index.js
+++ Lesson2/src/index.js
@@ -3,12 +3,12 @@ import ReactDom from 'react-dom';
 import App from './App';
 import Developer from './index2'
 
-ReactDom.render (
-    <App />,
+ReactDom.render( <
+    App / > ,
     document.querySelector('#web-page')
 );
 
-let a = new Developer ('alexey', '21');
+let a = new Developer('alexey', '21');
 
 a.setDeveloper();
 
