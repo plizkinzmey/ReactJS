926658d6c9713794cddc46ac61895644beaec285
Aleksandr Babkin <plizkinzmey@gmail.com>
2019-09-04 04:22:55 +0300

    project setup

--- Lesson5/src/App.jsx
+++ Lesson5/src/App.jsx
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Router, Route, IndexRoute, browserHistory, hashHistory} from 'react-router';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
-}
+import Layout from './app/layouts/Layout';
+import Main from './app/pages/Main';
+import PageNotFound from './app/pages/PageNotFound';
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+  <Route path="/" component={Layout}>
+    <IndexRoute component={Main}/>
+    <Route path="*" component={PageNotFound}></Route>
+  </Route>
+</Router>, document.getElementById('root'));
+
+// class App extends Component {   render() {     return (       <div></div> );
+// } }
 
 export default App;
